c85e28ee31450c14aeedf3a9ae8bef13d2096190
Kashchenko <rus.baranovskiy@gmail.com>
2023-01-16 03:34:44 +0300

    Added models: Character, Location, Episode

--- RickAndMorty/Models/RMEpisode.swift
+++ RickAndMorty/Models/RMEpisode.swift
@@ -1,8 +1,11 @@
-//
-//  RMEpisode.swift
-//  RickAndMorty
-//
-//  Created by Кащенко on 24.12.22.
-//
-
 import Foundation
+
+struct RMEpisode: Codable {
+    let id: Int
+    let name: String
+    let air_date: String
+    let episode: String
+    let characters: [String]
+    let url: String
+    let created: String
+}
--- RickAndMorty/Models/RMLocation.swift
+++ RickAndMorty/Models/RMLocation.swift
@@ -1,8 +1,11 @@
-//
-//  RMLocation.swift
-//  RickAndMorty
-//
-//  Created by Кащенко on 24.12.22.
-//
-
 import Foundation
+
+struct RMLocation: Codable {
+    let id: Int
+    let name: String
+    let type: String
+    let dimension: String
+    let residents: [String]
+    let url: String
+    let created: String
+}
